2fbe57815117990193dbe7bdd914ab0724dbe288
Valay Andhariya <andhariyavalay4@gmail.com>
2024-03-29 12:36:41 +0530

    Understaning how to interact with mesh(Objects)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+
 import "./App.css";
-import { useRef } from "react";
 
 const Cube = (props) => {
-  // Here useRef is use to give reference of mesh
   const meshRef = useRef();
 
-  // UseFrame is use to animate the objects , you can understand it as frames or also can see as roll of old cameras
-  // state refers to the state of object and delta is time difference between 2 frames
   useFrame((state, delta) => {
-    // It Rotates the Cube in x
     meshRef.current.rotation.x += delta;
-    // It Rotates the cube in y into 2.0
     meshRef.current.rotation.y += delta * 2.0;
-    // Here we move the postion of the cube here we move it in z and we use logic of the sin here as sin is moving in between -1 to 1 so it move cube to backward and forward
-    // And here understand that delta is very small value as it is difference of the frames
     meshRef.current.position.z = Math.sin(state.clock.elapsedTime);
   });
 
@@ -28,17 +22,30 @@ const Cube = (props) => {
 
 const Sphere = (props) => {
   const meshRef = useRef();
+  const [isHover, setIsHover] = useState(false);
+  const [isClick, setIsClick] = useState(false);
 
   useFrame((state, delta) => {
-    meshRef.current.rotation.x += delta;
-    meshRef.current.rotation.y += delta * 2.0;
-    meshRef.current.position.z = Math.sin(state.clock.elapsedTime);
+    // Using isHover here!
+    const speed = isHover ? 1 : 0.2;
+    meshRef.current.rotation.y += delta * speed;
   });
 
   return (
-    <mesh position={props.position} ref={meshRef}>
+    <mesh
+      position={props.position}
+      ref={meshRef}
+      // onPointerEnter means when cursor enters the mesh stopPropagation stops the underlying handlers to firing
+      onPointerEnter={(event) => (event.stopPropagation(), setIsHover(true))}
+      // onPointerLeave fires when cursor leaves the mesh!
+      onPointerLeave={() => setIsHover(false)}
+      // It is normal onClick event
+      onClick={() => setIsClick(!isClick)}
+      // scale can be increase or decrease the scale of the mesh repectvie to actual size!
+      scale={isClick ? 2 : 1}
+    >
       <sphereGeometry args={props.size} />
-      <meshStandardMaterial color={props.color} wireframe />
+      <meshStandardMaterial color={isHover ? "red" : props.color} wireframe />
     </mesh>
   );
 };
@@ -77,8 +84,6 @@ const ToursKnot = (props) => {
   );
 };
 
-// NOTE: There is many types of spheres are in Three js , you can see offical docs and interact with them plus you can also generate there your specific type of sizes so must go get must checkout three js offical docs!
-
 function App() {
   return (
     <>
@@ -86,9 +91,11 @@ function App() {
         <directionalLight position={[0, 0, 2]} />
 
         <group>
-          <Cube position={[1, 1, 0]} size={[1, 1, 1]} color={"red"} />
-          <Sphere position={[-1, 1, 0]} size={[1, 30, 30]} color={"blue"} />
-          <Tours
+          {/* <Cube position={[1, 1, 0]} size={[1, 1, 1]} color={"red"} /> */}
+
+          <Sphere position={[0, 0, 0]} size={[1, 30, 30]} color={"blue"} />
+
+          {/* <Tours
             position={[1, -1, 0]}
             size={[0.8, 0.3, 30, 30]}
             color={"green"}
@@ -97,7 +104,7 @@ function App() {
             position={[-1, -1, 0]}
             size={[0.8, 0.1, 1000, 50]}
             color={"hotPink"}
-          />
+          /> */}
         </group>
       </Canvas>
     </>
