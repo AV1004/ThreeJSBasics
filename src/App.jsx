6722b118a0d6e74becdf4c7b48ba6503ed0be27e
Valay Andhariya <andhariyavalay4@gmail.com>
2024-03-29 13:51:33 +0530

    understanding leva and drei packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { DirectionalLightHelper } from "three";
+import {
+  MeshWobbleMaterial,
+  OrbitControls,
+  useHelper,
+} from "@react-three/drei";
 
 import "./App.css";
+import { useControls } from "leva";
 
 const Cube = (props) => {
   const meshRef = useRef();
@@ -69,18 +76,83 @@ const Tours = (props) => {
 
 const ToursKnot = (props) => {
   const meshRef = useRef();
+  const toursKnotRef = useRef();
+
+  const { color, radius } = useControls({
+    color: "lightblue",
+    radius: {
+      value: 5,
+      min: 1,
+      max: 10,
+      step: 0.5,
+    },
+  });
 
   useFrame((state, delta) => {
-    meshRef.current.rotation.x += delta;
-    meshRef.current.rotation.y += delta * 2.0;
-    meshRef.current.position.z = Math.sin(state.clock.elapsedTime);
+    // meshRef.current.rotation.x += delta;
+    // meshRef.current.rotation.y += delta * 2.0;
+    // meshRef.current.position.z = Math.sin(state.clock.elapsedTime);
   });
 
   return (
     <mesh position={props.position} ref={meshRef}>
-      <torusKnotGeometry args={props.size} />
-      <meshStandardMaterial color={props.color} />
+      <torusKnotGeometry args={[radius, ...props.size]} ref={toursKnotRef} />
+      {/* <meshStandardMaterial color={props.color} /> */}
+      {/* meshWobbleMatrial is an effect , it kind of give wobble effect to your mesh */}
+      <MeshWobbleMaterial color={color} factor={1} speed={5} />
     </mesh>
+  );
+};
+
+const Scene = () => {
+  const directionalLightRef = useRef();
+
+  // Now to see wether our direcitonalLight is coming from we can use this hook of drei
+  // So the arguments are(refference of tag , we have to tell it which the component is (and this is imported from Three) , size , color)
+  useHelper(directionalLightRef, DirectionalLightHelper, 1, "White");
+
+  // This leva package is use to confiugure your objects and lights (any components)directly in broswer
+  const { lightColor, lightIntensity } = useControls({
+    lightColor: "white",
+    lightIntensity: {
+      value: 0.5,
+      min: 0,
+      max: 5,
+      step: 0.1,
+    },
+  });
+
+  return (
+    <>
+      <directionalLight
+        position={[0, 0, 2]}
+        ref={directionalLightRef}
+        color={lightColor}
+        intensity={lightIntensity}
+      />
+
+      <group>
+        {/* <Cube position={[1, 1, 0]} size={[1, 1, 1]} color={"red"} /> */}
+
+        {/* <Sphere position={[0, 0, 0]} size={[1, 30, 30]} color={"blue"} /> */}
+
+        {/* <Tours
+        position={[1, -1, 0]}
+        size={[0.8, 0.3, 30, 30]}
+        color={"green"}
+      /> */}
+
+        <ToursKnot
+          position={[0, 0, 0]}
+          size={[0.1, 1000, 50]}
+          color={"hotPink"}
+        />
+      </group>
+
+      {/* This OrbitControls named Tag is from package drei that is use in many ways like moving around with screen and many things */}
+      <OrbitControls />
+      {/* <OrbitControls enableZoom={false} /> */}
+    </>
   );
 };
 
@@ -88,24 +160,7 @@ function App() {
   return (
     <>
       <Canvas>
-        <directionalLight position={[0, 0, 2]} />
-
-        <group>
-          {/* <Cube position={[1, 1, 0]} size={[1, 1, 1]} color={"red"} /> */}
-
-          <Sphere position={[0, 0, 0]} size={[1, 30, 30]} color={"blue"} />
-
-          {/* <Tours
-            position={[1, -1, 0]}
-            size={[0.8, 0.3, 30, 30]}
-            color={"green"}
-          />
-          <ToursKnot
-            position={[-1, -1, 0]}
-            size={[0.8, 0.1, 1000, 50]}
-            color={"hotPink"}
-          /> */}
-        </group>
+        <Scene />
       </Canvas>
     </>
   );
