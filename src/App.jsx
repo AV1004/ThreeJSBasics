9bb47dc7d0abd68f85c6ac88e1e30788fdaa04b3
Valay Andhariya <andhariyavalay4@gmail.com>
2024-03-29 12:10:21 +0530

    Understanding Different types of Geometries

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,59 @@ const Cube = (props) => {
   );
 };
 
+const Sphere = (props) => {
+  const meshRef = useRef();
+
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta;
+    meshRef.current.rotation.y += delta * 2.0;
+    meshRef.current.position.z = Math.sin(state.clock.elapsedTime);
+  });
+
+  return (
+    <mesh position={props.position} ref={meshRef}>
+      <sphereGeometry args={props.size} />
+      <meshStandardMaterial color={props.color} wireframe />
+    </mesh>
+  );
+};
+
+const Tours = (props) => {
+  const meshRef = useRef();
+
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta;
+    meshRef.current.rotation.y += delta * 2.0;
+    meshRef.current.position.z = Math.sin(state.clock.elapsedTime);
+  });
+
+  return (
+    <mesh position={props.position} ref={meshRef}>
+      <torusGeometry args={props.size} />
+      <meshStandardMaterial color={props.color} />
+    </mesh>
+  );
+};
+
+const ToursKnot = (props) => {
+  const meshRef = useRef();
+
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta;
+    meshRef.current.rotation.y += delta * 2.0;
+    meshRef.current.position.z = Math.sin(state.clock.elapsedTime);
+  });
+
+  return (
+    <mesh position={props.position} ref={meshRef}>
+      <torusKnotGeometry args={props.size} />
+      <meshStandardMaterial color={props.color} />
+    </mesh>
+  );
+};
+
+// NOTE: There is many types of spheres are in Three js , you can see offical docs and interact with them plus you can also generate there your specific type of sizes so must go get must checkout three js offical docs!
+
 function App() {
   return (
     <>
@@ -34,9 +87,17 @@ function App() {
 
         <group>
           <Cube position={[1, 1, 0]} size={[1, 1, 1]} color={"red"} />
-          <Cube position={[-1, 1, 0]} size={[1, 1, 1]} color={"blue"} />
-          <Cube position={[1, -1, 0]} size={[1, 1, 1]} color={"green"} />
-          <Cube position={[-1, -1, 0]} size={[1, 1, 1]} color={"orange"} />
+          <Sphere position={[-1, 1, 0]} size={[1, 30, 30]} color={"blue"} />
+          <Tours
+            position={[1, -1, 0]}
+            size={[0.8, 0.3, 30, 30]}
+            color={"green"}
+          />
+          <ToursKnot
+            position={[-1, -1, 0]}
+            size={[0.8, 0.1, 1000, 50]}
+            color={"hotPink"}
+          />
         </group>
       </Canvas>
     </>
